00711a0660ed6afc3398db153c9f0ed8dfd9a932
Matthew Blott <m4ttb@pm.me>
2023-02-22 14:24:36 +0000

    saved

---
+++ src/App.svelte
@@ -0,0 +1,36 @@
+<script lang="ts">
+  import { Capacitor } from "@capacitor/core";
+  import { SafeArea } from "capacitor-plugin-safe-area";
+
+  $: display = false;
+
+  const toggleMessage = () => {
+    display = !display;
+
+    const platform = Capacitor.getPlatform();
+
+    console.log(platform);
+
+    SafeArea.getStatusBarHeight().then(({ statusBarHeight }) => {
+      console.log(statusBarHeight, "statusbarHeight");
+    });
+
+    SafeArea.getSafeAreaInsets().then(({ insets }) => {
+      console.log(insets);
+    });
+  };
+</script>
+
+<main>
+  <button on:click={toggleMessage}>Toggle Message</button>
+
+  {#if display}
+    <h1>Hello World</h1>
+  {/if}
+</main>
+
+<style>
+  main {
+    margin-top: 100px;
+  }
+</style>
